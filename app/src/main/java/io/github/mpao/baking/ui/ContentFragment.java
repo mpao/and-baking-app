7fdf5eb3a5402cfea2ef85b167b75f1d1c874873
marco <mpao@me.com>
2018-03-20 23:24:43 +0100

    implement ExoPlayer

--- app/src/main/java/io/github/mpao/baking/ui/ContentFragment.java
+++ app/src/main/java/io/github/mpao/baking/ui/ContentFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +12,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import io.github.mpao.baking.R;
 import io.github.mpao.baking.databinding.FragmentContentBinding;
 import io.github.mpao.baking.di.App;
@@ -23,6 +34,7 @@ public class ContentFragment extends Fragment {
     private Activity activity;
     private Recipe recipe;
     private int position;
+    private SimpleExoPlayer player;
 
     /*
      * check the @Nullable value returned by getActivity()
@@ -58,6 +70,7 @@ public class ContentFragment extends Fragment {
 
     @Override
     public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+
         super.onViewStateRestored(savedInstanceState);
         if(savedInstanceState != null){
             update(
@@ -65,6 +78,18 @@ public class ContentFragment extends Fragment {
                     savedInstanceState.getInt(App.STEP_INDEX)
             );
         }
+
+    }
+
+    /*
+     * Release the player
+     */
+    @Override
+    public void onStop() {
+
+        super.onStop();
+        releaseVideoPlayer();
+
     }
 
     @Override
@@ -93,8 +118,45 @@ public class ContentFragment extends Fragment {
             this.position = position;
             this.recipe = recipe;
             Step step = recipe.getSteps().get(position);
+            setUpVideoPlayer(step);
+
             binding.setStep(step);
         }
+
+    }
+
+    /*
+     * Set up and start the video
+     */
+    private void setUpVideoPlayer(Step step){
+
+        if(player != null) {
+            releaseVideoPlayer();
+        }
+        TrackSelector trackSelector = new DefaultTrackSelector();
+        LoadControl loadControl = new DefaultLoadControl();
+        player = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+        binding.player.setPlayer(player);
+        MediaSource mediaSource = new ExtractorMediaSource(
+                Uri.parse(step.getVideoURL()),
+                new DefaultDataSourceFactory(activity, activity.getString(R.string.app_name)),
+                new DefaultExtractorsFactory(),
+                null,
+                null
+        );
+        player.prepare(mediaSource);
+        player.setPlayWhenReady(true); //todo network error
+
+    }
+
+    /*
+     * Release and destroy the video
+     */
+    private void releaseVideoPlayer(){
+
+        player.stop();
+        player.release();
+        player = null;
 
     }
 
