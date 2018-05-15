23f0f66a140473522daeae9e997ce0f12d3b3a87
marco <mpao@me.com>
2018-05-15 21:50:58 +0200

    add dummy test

--- app/src/androidTest/java/io/github/mpao/baking/LayoutTest.java
+++ app/src/androidTest/java/io/github/mpao/baking/LayoutTest.java
@@ -1,13 +1,18 @@
 package io.github.mpao.baking;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import io.github.mpao.baking.ui.MainActivity;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.AllOf.allOf;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,12 +21,17 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class LayoutTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("io.github.mpao.baking", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void selectRecipe() {
+
+        onView( allOf( withId(R.id.list) ) ).check(matches( isDisplayed() ) );
+        onView( allOf( withId(R.id.list) ) ).perform(actionOnItemAtPosition(0, click() ) );
+        onView( allOf( withId(R.id.list) ) ).check( matches(isDisplayed()) );
+
     }
 
 }
